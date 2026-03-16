c83a41c0d3dc9c8d410b2cc98b402316e008ba63
nemanjastrasni <nemanjaivanovic979@gmail.com>
2026-03-16 23:18:34 +0100

    fix nextauth prisma adapter

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,49 +1,51 @@
 import GitHubProvider from "next-auth/providers/github"
-import type { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import type { NextAuthOptions } from "next-auth"
 import bcrypt from "bcrypt"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { prisma } from "@/lib/prisma"
 
 const adminEmails = process.env.ADMIN_EMAILS?.split(",") ?? []
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+
     CredentialsProvider({
-  name: "Credentials",
-  credentials: {
-    email: {},
-    password: {}
-  },
-  async authorize(credentials) {
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {}
+      },
+      async authorize(credentials) {
 
-    const user = await prisma.user.findUnique({
-      where: { email: credentials?.email }
-    })
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email }
+        })
 
-    if (!user || !user.password) return null
+        if (!user || !user.password) return null
 
-    const valid = await bcrypt.compare(
-      credentials!.password,
-      user.password
-    )
+        const valid = await bcrypt.compare(
+          credentials!.password,
+          user.password
+        )
 
-    if (!valid) return null
+        if (!valid) return null
 
-    return user
-  }
-}),
+        return user
+      }
+    }),
   ],
-  
 
   secret: process.env.NEXTAUTH_SECRET,
 
   callbacks: {
     async jwt({ token, user }) {
-      
 
       if (user?.email) {
         token.role = adminEmails.includes(user.email)
@@ -55,8 +57,6 @@ export const authOptions: NextAuthOptions = {
         token.role = "user"
       }
 
-      
-
       return token
     },
 
@@ -64,8 +64,6 @@ export const authOptions: NextAuthOptions = {
       if (session.user) {
         session.user.role = token.role as "admin" | "user"
       }
-
-      
 
       return session
     },
