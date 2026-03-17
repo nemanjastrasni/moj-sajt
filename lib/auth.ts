46b176f2342452fa67e82fd2ed19cd9b5052e9ce
nemanjastrasni <nemanjaivanovic979@gmail.com>
2026-03-17 20:44:50 +0100

    remove github use credentials only

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,19 +1,38 @@
-import GitHubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
 import type { NextAuthOptions } from "next-auth"
+import bcrypt from "bcrypt"
+import { prisma } from "@/lib/prisma"
 
 export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        })
+
+        if (!user || !user.password) return null
+
+        const valid = await bcrypt.compare(
+          credentials!.password,
+          user.password
+        )
+
+        if (!valid) return null
+
+        return user
+      },
     }),
   ],
 
   secret: process.env.NEXTAUTH_SECRET,
-
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      return baseUrl
-    },
-  },
 }
