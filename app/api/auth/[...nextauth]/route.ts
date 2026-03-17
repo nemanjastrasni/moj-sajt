a5388a6025cdb9c2844e38ab4098ef3c60a96e00
nemanjastrasni <nemanjaivanovic979@gmail.com>
2026-03-17 20:55:07 +0100

    debug nextauth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import { authOptions } from "@/lib/auth"
-console.log("AUTH LOADED")
 
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
+console.log("NEXTAUTH ROUTE ACTIVE")
