caab7f5dcf39b433a296f6caa9eb65055431196f
nemanjastrasni <nemanjaivanovic979@gmail.com>
2026-03-18 22:33:00 +0100

    fix prisma env

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,18 +4,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL)
-console.log("DIRECT_URL:", process.env.DIRECT_URL)
-
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["error", "query"],
-    datasources: {
-      db: {
-        url: "postgresql://neondb_owner:npg_s5w2VvETlnmX@ep-wispy-dawn-alnxam1d-pooler.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-      },
-    },
+    log: ["error"],
   })
 
 if (process.env.NODE_ENV !== "production") {
