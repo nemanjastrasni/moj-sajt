921568840d150256a8460b198070d9d10bbcc428
nemanjastrasni <nemanjaivanovic979@gmail.com>
2026-03-18 18:34:44 +0100

    final prisma fix for supabase pooler

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,4 @@
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = globalThis as unknown as {
@@ -8,11 +9,7 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ["error"],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
+    
   })
 
 if (process.env.NODE_ENV !== "production") {
