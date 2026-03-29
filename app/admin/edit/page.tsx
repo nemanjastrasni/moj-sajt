429ca08325513ca3d44a19c4af5ba8fdcbfd378e
nemanjastrasni <nemanjaivanovic979@gmail.com>
2026-03-29 16:05:37 +0200

    fix:filter crash2

--- app/admin/edit/page.tsx
+++ app/admin/edit/page.tsx
@@ -1,3 +1,56 @@
-export default function AdminEdit() {
-  return <div>ADMIN OK</div>
+import { prisma } from "@/lib/prisma"
+
+export default async function AdminEdit({ searchParams }: any) {
+  
+  const artists = await prisma.artist.findMany({
+    select: { id: true, name: true }
+  })
+
+  const songs = await prisma.song.findMany({
+    take: 10
+  })
+
+  return (
+    <div className="p-6">
+
+      <h1 className="text-2xl font-bold mb-6">Edit tabela</h1>
+
+      <table className="w-full text-sm text-black">
+
+        <thead className="sticky top-0 bg-white z-10">
+          <tr className="text-left border-b border-gray-700">
+            <th>Title</th>
+            <th>Slug</th>
+            <th>ArtistId</th>
+            <th>Category</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {songs.map((s) => (
+            <tr key={s.id} className="border-b border-gray-800">
+
+              <td>{s.title}</td>
+              <td>{s.slug}</td>
+              <td>{s.artistId}</td>
+              <td>{s.category}</td>
+
+              <td>
+                <a
+                  href={`/admin/songs/${s.id}`}
+                  className="text-blue-400 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+
+    </div>
+  )
 }
