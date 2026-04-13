74b28e53916d126adcb70d082b624c4670bf5a62
nemanjastrasni <nemanjaivanovic979@gmail.com>
2026-04-13 20:52:43 +0200

    fix POST playlist (405 error)

--- app/api/listening-playlist/route.ts
+++ app/api/listening-playlist/route.ts
@@ -1,38 +1,24 @@
 import { prisma } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
-
 export async function POST(req: Request) {
   try {
     const formData = await req.formData()
     const name = formData.get("name") as string
 
     if (!name) {
-      return NextResponse.json(
-        { error: "Missing name" },
-        { status: 400 }
-      )
+      return NextResponse.json({ error: "Missing name" }, { status: 400 })
     }
 
-    const playlist = await prisma.listeningPlaylist.create({
-      data: {
-        name,
-      },
+    await prisma.listeningPlaylist.create({
+      data: { name },
     })
 
     return NextResponse.redirect(
-  new URL("/listening-playlist", req.url)
-)
-  } catch (error) {
-    console.error("PLAYLIST ERROR FULL:", error)
-
-    return NextResponse.json(
-      { error: "Server error" },
-      { status: 500 }
+      new URL("/listening-playlist", req.url)
     )
+  } catch (error) {
+    console.error("CREATE ERROR:", error)
+    return NextResponse.json({ error: "Server error" }, { status: 500 })
   }
-}
-
-export async function GET() {
-  return NextResponse.json({ ok: true })
 }
